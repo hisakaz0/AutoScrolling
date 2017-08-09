5e81a7fa18d346f650feb559b6a56bd54ae62d1f
pinkienort <cantabilehisa@gmail.com>
2017-08-09 15:53:52 +0900

    fix bugs: stop auto-scrolling when the activeWindow of firefox switch to another firefox window

--- background.js
+++ background.js
@@ -3,19 +3,23 @@
 
 let browser_action = {
   isScroll: false,
-  tid: -1
+  tid: -1,
+  wid: -1
 };
 
 browser.browserAction.onClicked.addListener((tab) => {
   browser_action.isScroll = !browser_action.isScroll;
   browser_action.tid = tab.id;
+  browser_action.wid = tab.windowId;
   browser.tabs.sendMessage(tab.id,
       {isScroll: browser_action.isScroll});
 });
 
-browser.tabs.onActivated.addListener(function (tabId, windowId) {
-  if (browser_action.isScroll) {
-    browser.tabs.sendMessage(browser_action.tid, {isScroll: false});
+browser.tabs.onActivated.addListener(function (activeInfo) {
+  if (browser_action.isScroll &&
+    browser_action.wid == activeInfo.windowId) {
+    browser.tabs.sendMessage(
+      browser_action.tid, {isScroll: false});
+    browser_action.isScroll = false;
   }
-  browser_action.isScroll = false;
 });

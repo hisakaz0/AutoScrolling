9b3f9aec8e5d7acdcad15f91a281a5f9bcfab698
Hisakazu Fukuoka <cantabilehisa@gmail.com>
2018-09-24 23:23:31 +0900

    chore: Insall test env (#38)
    - Install karma, jasmine
    - Possible to treat html
    
    related issue: #33

--- tests/kill-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kill 30702
--- tests/run-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-php -S localhost:8080 1>/dev/null 2>&1 &
-pid=$!
-cat /dev/null > kill-server.sh
-echo "#!/bin/bash" >> kill-server.sh
-echo "kill $pid" >> kill-server.sh
-chmod 755 kill-server.sh
-

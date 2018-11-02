a5996768d0138da05981a695b42607e929a2b9f9
Hisakazu Fukuoka <cantabilehisa@gmail.com>
2018-11-02 10:01:30 +0900

    Create menus.js

---
+++ src/modules/browser/menus.js
@@ -0,0 +1,18 @@
+
+const createContextMenu = (props, callback) => {
+  return browser.meuns.create(menu, callback);
+};
+
+const updateContextMenu = (id, props) => {
+  return browser.menus.update(id, props);
+};
+
+const addOnClickListener = listener => {
+  return browser.menus.onClicked(listener);
+};
+
+export {
+  createContextMenu,
+  updateContextMenu,
+  addOnClickListener
+};

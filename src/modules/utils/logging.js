14a1c850165307371724f8ce666168c69511b55e
pinkienort <cantabilehisa@gmail.com>
2018-11-04 17:15:50 +0900

    feat: imple debug mode
    - Add logger
    - Add debug flag

--- src/modules/utils/logging.js
+++ src/modules/utils/logging.js
@@ -1,7 +1,31 @@
 "use strict";
 
-const onError = err => {
-  console.error(`Autoscrolling Error: ${err}`);
-};
+class Logger {
+  constructor() {
+    this.DEFAULT_TAG_NAME = "AutoScrolling";
+  }
 
-export default onError;
+  argsGetter(tag, message) {
+    if (typeof message === typeof undefined) {
+      message = tag;
+      tag = this.DEFAULT_TAG_NAME;
+    }
+    return [tag, message];
+  }
+
+  logging(func, tag, message) {
+    func.apply(argsGetter(tag, message));
+  }
+
+  debug(tag, message) {
+    if (!process.env.DEBUG) return;
+    this.logging(console.debug, tag, message);
+  }
+
+  error(tag, message) {
+    this.logging(console.error, tag, message);
+  }
+}
+const logger = new Logger();
+
+export { logger };

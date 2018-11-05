6ab2fccbf827b080e4c0a5894acefc624eb0c12e
pinkienort <cantabilehisa@gmail.com>
2018-11-05 21:22:34 +0900

    feat: restore scrolling from switch back
    - Add new option: restore scrolling from switch back
    - Refactor background script
    - Add EventType into action func
    - Update package-lock.json
    
    related: #64

---
+++ src/scripts/background/event.js
@@ -0,0 +1,13 @@
+const EventType = {
+  EVENT_ID_NONE: -1,
+  SINGLE_CLICK: 1,
+  DOUBLE_CLICK: 2,
+  TAB_CHANGED: 3,
+  TAB_UPDATED: 4,
+  TAB_REMOVED: 5,
+  WINDOW_CHANGED: 6,
+  CONTENT_SCRIPT_MESSAGE: 7,
+  INIT_CONTENT_SCRIPT: 8
+};
+
+export { EventType };

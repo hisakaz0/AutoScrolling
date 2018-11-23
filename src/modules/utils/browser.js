e79097841392ec4f68b561b5fa050b7815e5adb3
pinkienort <cantabilehisa@gmail.com>
2018-11-24 00:20:01 +0900

    refactor: chrome support
    - install Object.fromEntries polyfill
    - refacotr error handling

--- src/modules/utils/browser.js
+++ src/modules/utils/browser.js
@@ -1,9 +1,12 @@
 import { getTab } from '../browser/index';
 import { isSystemProtocol } from './util';
 
-const isNotSystemTabWith = tabId => getTab(tabId).then((tab) => {
-  if (isSystemProtocol(tab.url)) return Promise.reject();
-  return Promise.resolve();
-});
+const isNotSystemTabWith = tabId => getTab(tabId).then(tab => new Promise((resolve, reject) => {
+  if (isSystemProtocol(tab.url)) {
+    reject(new Error(`Provided url seems like system: ${tab.url}`));
+  } else {
+    resolve(true);
+  }
+}));
 
 export default isNotSystemTabWith;
